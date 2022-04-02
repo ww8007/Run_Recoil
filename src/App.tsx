79943caed3d99c197739f66023461cfed7747c96
ww8007 <wshmin@naver.com>
2022-04-02 23:31:48 +0900

    Feat: add QueryClient setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { CharacterCounter } from './components/counter';
 import { TodoList } from './components/TodoList';
+import { QueryClient, QueryClientProvider } from 'react-query';
 export default function App() {
+	const queryClient = new QueryClient();
 	return (
 		<RecoilRoot>
-			<TodoList />
+			<QueryClientProvider client={queryClient}>
+				<TodoList />
+			</QueryClientProvider>
 		</RecoilRoot>
 	);
 }
