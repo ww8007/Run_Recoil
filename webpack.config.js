635586db9ccf69c043ae9bfad3f06c5c6fdb4a06
ww8007 <wshmin@naver.com>
2022-04-02 23:02:21 +0900

    Chore: remove not use plugins

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,7 @@ module.exports = {
 						'@babel/preset-react',
 						'@babel/preset-typescript'
 					],
-					plugins: [
-						['@babel/plugin-proposal-class-properties', { loose: true }],
-						['@babel/plugin-proposal-private-methods', { loose: true }],
-						['@babel/plugin-transform-runtime']
-					]
+					plugins: [['@babel/plugin-transform-runtime']]
 				}
 			}
 		]
